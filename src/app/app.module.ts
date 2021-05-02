03c6f928c1f09d3f644872ff1384c731195a0ecd
Alok Ashtikar <alok@elementalconcept.com>
2021-05-02 11:36:03 +0100

    cognito ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,78 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+/* Add Amplify imports */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+import Amplify, { Auth } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+
+    // REQUIRED - Amazon Cognito Region
+    region: 'ap-south-1',
+
+    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+    // Required only if it's different from Amazon Cognito Region
+    // identityPoolRegion: 'ap-south-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'ap-south-1_Gr2nfvGU1',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '63kol9gnuuisjipb993endqim5',
+
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: false,
+
+    // OPTIONAL - Configuration for cookie storage
+    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+    // cookieStorage: {
+    //   // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+    //   domain: '.yourdomain.com',
+    //   // OPTIONAL - Cookie path
+    //   path: '/',
+    //   // OPTIONAL - Cookie expiration in days
+    //   expires: 365,
+    //   // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+    //   // sameSite: "strict" | "lax",
+    //   // OPTIONAL - Cookie secure flag
+    //   // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+    //   secure: true
+    // },
+
+    // OPTIONAL - customized storage object
+    // storage: MyStorage,
+
+    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+
+    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+    // clientMetadata: { myCustomKey: 'myCustomValue' },
+
+    // OPTIONAL - Hosted UI configuration
+    oauth: {
+      domain: 'sparedevice-dev.auth.ap-south-1.amazoncognito.com',
+      // scope: ['aws.cognito.signin.user.admin'],
+      redirectSignIn: 'http://localhost:4200/cognito',
+      redirectSignOut: 'http://localhost:4200/',
+      responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+    }
+  }
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure();
+
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    AmplifyUIAngularModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
